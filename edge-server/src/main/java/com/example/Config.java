f5704fb4f076f8fd79c0f80de05da36402c9d38f
Ketan Gote <ketan.gote@metamagic.in>
2017-02-22 21:13:54 +0530

    Zulu configuration

---
+++ edge-server/src/main/java/com/example/Config.java
@@ -0,0 +1,12 @@
+package com.metamagic;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableZuulProxy
+public class Config {
+
+}
